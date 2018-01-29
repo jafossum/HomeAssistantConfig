3b81e5bddf642a4cd67d190958737a93bf76f547
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2018-01-29 22:57:48 +0000

    Adding .sh for creating docker container

---
+++ docker/docker_run.sh
@@ -0,0 +1,9 @@
+docker run -d \
+--name="home-assistant" \
+-v $(pwd)/..:/config \
+-v /etc/localtime:/etc/localtime:ro \
+-v /etc/letsencrypt:/etc/letsencrypt:ro \
+-v /usr/bin/ssl-cert-check:/usr/bin/ssl-cert-check:ro \
+--device /dev/ttyACM0:/dev/ttyACM0 \
+--net=host \
+homeassistant/raspberrypi3-homeassistant
