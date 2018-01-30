b713c63fa9e248639d4a5872c29389ae458c3793
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2018-01-30 22:18:55 +0000

    standard config path. Adding autoremove --rm

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,17 +1,10 @@
 docker run -d \
 --name="home-assistant" \
--v $(pwd)/..:/config \
+-v /home/homeassistant/.homeassistant:/config \
 -v /etc/localtime:/etc/localtime:ro \
 -v /etc/letsencrypt:/etc/letsencrypt:ro \
 -v /usr/bin/ssl-cert-check:/usr/bin/ssl-cert-check:ro \
 --device /dev/ttyACM0:/dev/ttyACM0 \
 --net=host \
+--rm \
 homeassistant/raspberrypi3-homeassistant
-
-#docker run -d \
-#--name="home-assistant" \
-#-v $(pwd)/..:/config \
-#-v /etc/localtime:/etc/localtime:ro \
-#--device /dev/ttyACM0:/dev/ttyACM0 \
-#--net=host \
-#jafossum/home-assistant:latest
